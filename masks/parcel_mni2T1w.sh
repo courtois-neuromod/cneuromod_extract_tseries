f40b79b592a44f1e7910b17ea74ff1d0032c3d9c
MarieStLaurent <marie.stl@gmail.com>
2024-01-31 12:43:38 -0500

    mask scripts

--- masks/parcel_mni2T1w.sh
+++ masks/parcel_mni2T1w.sh
@@ -7,13 +7,14 @@ module load ants/2.3.5
 
 MNIPARCEL="${1}"  # /path/to/infile_MNI.nii
 OUTPATH="${2}"  # /path/to/output/dir
+PNAME="${3}"  # / kanwisher-body-EBA
 SPREPDIR="/home/mstlaure/projects/rrg-pbellec/mstlaure/cneuromod_extract_tseries/data/friends.fmriprep/sourcedata/smriprep"
 
 for SUBNUM in 01 02 03 04 05 06
 do
   antsApplyTransforms --default-value 0 --dimensionality 3 --float 0 \
   --input ${MNIPARCEL} --interpolation Linear \
-  --output ${OUTPATH}/sub-${SUBNUM}_test.nii \
+  --output ${OUTPATH}/sub-${SUBNUM}_${PNAME}_t1w.nii.gz \
   --reference-image ${SPREPDIR}/sub-${SUBNUM}/anat/sub-${SUBNUM}_desc-preproc_T1w.nii.gz \
   --transform ${SPREPDIR}/sub-${SUBNUM}/anat/sub-${SUBNUM}_from-MNI152NLin2009cAsym_to-T1w_mode-image_xfm.h5
 done
